11ec882f344fd295a4aa81f5559077c911e827d3
Vanessa Jimenez <jvanessa300@gmail.com>
2026-02-24 10:03:30 -0700

    Wrote the code for Prep1. Final and working.

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,19 @@
 using System;
 
-class Program
+class Program       
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // Console.WriteLine("Hello Prep1 World!");
+        string firstName;
+        string lastName;
+
+        Console.Write("What is your first name: ");
+        firstName = Console.ReadLine();
+
+        Console.Write("What is your last name: ");
+        lastName = Console.ReadLine();
+
+        Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}");
     }
 }
