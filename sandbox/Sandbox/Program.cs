857cd7c41c161d5e222b2af46981dd8d34c37db6
Vanessa Jimenez <jvanessa300@gmail.com>
2026-02-25 21:19:56 -0700

    Prep 1 Assignment

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -5,5 +5,28 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Sandbox World!");
+
+        int age;
+
+        Console.Write("Please input your age: ");
+        // string response = Console.ReadLine();
+       // age = int.Parse(response);    
+
+        age = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Your age is: {age}");
+
+        if (age <= 100)
+        {
+            Console.WriteLine("You are cool.");
+        }
+        else if (age <= 150)
+        {
+            Console.WriteLine("You are sort of cool.");
+        }
+        else
+        {
+            Console.WriteLine("Wow, You've had a lot of Birthdays!");
+        }
     }
 }
